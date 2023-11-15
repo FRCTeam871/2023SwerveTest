8695f89141ba892506132a7fed19569ab19c36bf
871swlaptop <abaranec3@gmail.com>
2023-11-14 20:03:12 -0500

    Adds PID controller control

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 
@@ -33,6 +34,7 @@ public class Robot extends TimedRobot {
   private CANCoder sensor4;
   private XboxController xboxController;
   private long timeMs=System.currentTimeMillis();
+  private PIDController module1Steer;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -42,21 +44,27 @@ public class Robot extends TimedRobot {
 
       driveMotor1 = new CANSparkMax(12, MotorType.kBrushless);
       steeringMotor1 = new CANSparkMax(10,MotorType.kBrushless);
+      steeringMotor1.setInverted(true);
       sensor1 = new CANCoder(11);
-
+      sensor1.configMagnetOffset(0);
+      
       driveMotor2 = new CANSparkMax(9, MotorType.kBrushless);
       steeringMotor2 = new CANSparkMax(7,MotorType.kBrushless);
       sensor2 = new CANCoder(8);
+      sensor2.configMagnetOffset(282.22);
 
       driveMotor3 = new CANSparkMax(6, MotorType.kBrushless);
       steeringMotor3 = new CANSparkMax(4,MotorType.kBrushless);
       sensor3 = new CANCoder(5);
+      sensor3.configMagnetOffset(53);
 
       driveMotor4 = new CANSparkMax(3, MotorType.kBrushless);
       steeringMotor4 = new CANSparkMax(1,MotorType.kBrushless);
       sensor4 = new CANCoder(2);
-      
+      sensor4.configMagnetOffset(54.32);
+
       xboxController = new XboxController(0);
+      module1Steer = new PIDController(1.0/360.0, 1.0/3600, 0);
 
   }
 
@@ -81,19 +89,26 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    double drivePower = xboxController.getLeftY();  
-    double steeringPower = xboxController.getRightX();
-    driveMotor1.set(drivePower);
-    steeringMotor1.set(steeringPower);
-    driveMotor2.set(drivePower);
-    steeringMotor2.set(steeringPower);
-    driveMotor3.set(drivePower);
-    steeringMotor3.set(steeringPower);
-    driveMotor4.set(drivePower);
-    steeringMotor4.set(steeringPower);
+   // double drivePower = xboxController.getLeftY();  
+   // double steeringPower = 0; //xboxController.getRightX();
+   // driveMotor1.set(drivePower);
+    //steeringMotor1.set(steeringPower);
+   // driveMotor2.set(drivePower);
+   // steeringMotor2.set(steeringPower);
+   // driveMotor3.set(drivePower);
+   // steeringMotor3.set(steeringPower);
+    //driveMotor4.set(drivePower);
+   //steeringMotor4.set(steeringPower);
+
+   double setpoint = Math.atan2(xboxController.getLeftX(), xboxController.getLeftY())*180/Math.PI;
+
+   double position = sensor1.getAbsolutePosition();
+   double output = module1Steer.calculate(position, setpoint);
+   steeringMotor1.set(output);
+
     if(System.currentTimeMillis()-timeMs>100) {
       timeMs=System.currentTimeMillis();
-      System.out.println("position:" + sensor1.getPosition() + " position2:" + sensor2.getPosition() + " position3:" + sensor3.getPosition() + " position4:" + sensor4.getPosition() ); 
+      System.out.println("position:" + sensor1.getAbsolutePosition() + " position2:" + sensor2.getAbsolutePosition() + " position3:" + sensor3.getAbsolutePosition() + " position4:" + sensor4.getAbsolutePosition() + "setpoint=" + setpoint); 
     }
     
   }
